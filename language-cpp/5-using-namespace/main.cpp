83d462da0d7747bbbd7c98744965786a3e49baed
Tairon <Official.Teren@yandex.ru>
2024-10-05 19:04:12 +0300

    CPP-7: a folder (using-namespace) has been added

---
+++ language-cpp/5-using-namespace/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+// или 
+// using namespace std;
+// для всего 
+
+int main() {
+	cout << "123" << endl;
+	return 0;
+}
